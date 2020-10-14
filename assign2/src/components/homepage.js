10cf735b85233ff697ceabcf188875f04246b61e
argorn-inc <70586243+argorn-inc@users.noreply.github.com>
2020-10-14 21:44:43 +0000

    Update homepage.js

--- assign2/src/components/homepage.js
+++ assign2/src/components/homepage.js
@@ -1,17 +1,15 @@
-import React, {useSate, useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './component.css';
 import today from '../img/today.jpg';
 import imageHeader from './bg.jpg';
-// import bg from './../../public/bg.jpg';
-
-// const [temperature,setTemperature ]= useState(29);
-
-//  function Today(){
-//     const [temperature,setTemperature ]= useState(29);
-//  };
 
 function Home(){
     const [temperature,setTemperature ]= useState(29);
+    useEffect(
+    ()=> {
+        document.title=`Temperature now: ${temperature}`;
+    }
+    );
     return(
         <div className="home">
             <div className="site-title">
